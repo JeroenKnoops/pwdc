bef2091b60aa6c1c22a634db4d7d6487bc0fbdbc
Jeroen Knoops <jeroen.knoops@philips.com>
2024-11-14 15:58:22 +0100

    Use flake instead of default.nix
    Signed-off-by: Jeroen Knoops <jeroen.knoops@philips.com>

--- flake.nix
+++ flake.nix
@@ -10,15 +10,33 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        buildInputs = [ 
+          pkgs.cargo 
+          pkgs.libiconv
+          pkgs.darwin.apple_sdk.frameworks.AppKit
+        ];
+        manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
       in
       {
         devShells.default = pkgs.mkShell {
-          buildInputs = [ 
-            pkgs.cargo 
-            pkgs.libiconv
-            pkgs.darwin.apple_sdk.frameworks.AppKit
-          ];
+          buildInputs = buildInputs; 
         };
-      }
-    );
+
+  packages.default = pkgs.rustPlatform.buildRustPackage rec {
+    buildInputs = [ 
+      pkgs.darwin.apple_sdk.frameworks.AppKit
+    ];
+    pname = manifest.name;
+    version = manifest.version;
+    cargoLock.lockFile = ./Cargo.lock;
+    src = pkgs.lib.cleanSource ./.;
+    meta = with pkgs.lib; {
+      description = "This tool will take the current directory and puts it on the clipboard.";
+      homepage = "https://github.com/jeroenkoops/pwdc";
+      license = licenses.mit;
+      maintainers = [ jeroenknoops ];
+    };
+  };
+  
+} );
 }
