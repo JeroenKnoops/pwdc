ef2bfd23963d6c10d65257cca4124c867d132fb2
Jeroen Knoops <jeroen.knoops@philips.com>
2025-10-19 12:36:21 +0200

    home-manager assert
    Signed-off-by: Jeroen Knoops <jeroen.knoops@philips.com>

--- home-module.nix
+++ home-module.nix
@@ -1,10 +1,11 @@
 { config, lib, pkgs, pwdcPackage ? null, ... }:
 
+assert pwdcPackage != null;
+
 {
   options.programs.pwdc.enable = lib.mkEnableOption "Enable the pwdc CLI tool";
 
   config = lib.mkIf config.programs.pwdc.enable {
-    assert pwdcPackage != null;
     home.packages = [ pwdcPackage ];
   };
 }
